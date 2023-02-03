62168eb34f695a86f0d7eb983575885766c4193f
anjekova <anjekova@gmail.com>
2023-02-03 13:49:00 +0200

    Test Product stream

--- src/__tests__/unit/components/case-4-ProductStream.test.tsx
+++ src/__tests__/unit/components/case-4-ProductStream.test.tsx
@@ -3,8 +3,41 @@ import { axe } from 'jest-axe'
 import { render } from '@testing-library/react'
 import ProductStream from '../../../components/ProductStream'
 
-describe('The <ProductStream /> component', () => {
-  it('❌ renders a list of Product tiles for each product passed to it', async () => {})
+const defaultProps = [
+  {
+    id: 12,
+    name: 'Adidas 1',
+    image: './image.png',
+    price: 'from $12.99',
+    brand: 'Adidas',
+    createdAt: '2020-02-11 00:00:00',
+    isActive: true,
+  },
+  {
+    id: 13,
+    name: 'Nike 1',
+    image: './image-1.png',
+    price: 'from $42.99',
+    brand: 'Nike',
+    createdAt: '2020-02-11 00:00:00',
+    isActive: true,
+  },
+];
 
-  it('❌ has no accessibility violations', async () => {})
+describe('The <ProductStream /> component', () => {
+  const setupProductStream = () => render(<ProductStream products={defaultProps as any} />);
+
+  it('❌ renders a list of Product tiles for each product passed to it', async () => {
+    const { getAllByTestId, debug } = setupProductStream();
+
+    const productItems = getAllByTestId('ProductTile')
+
+    expect(productItems).toHaveLength(defaultProps.length)
+  });
+
+  it('❌ has no accessibility violations', async () => {
+    const { container } = setupProductStream();
+
+    expect(await axe(container)).toHaveNoViolations();
+  })
 })
